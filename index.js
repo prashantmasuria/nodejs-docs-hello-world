9bed8bfe3938fae661eaaed2962c11759febe65a
Prashant Masuria <prashantmasuria@gmail.com>
2019-02-01 19:16:22 +0530

    update index

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ var server = http.createServer(function(request, response) {
 
 var port = process.env.PORT || 1337;
 server.listen(port);
+console.log("hello world");
 
 console.log("Server running at http://localhost:%d", port);
